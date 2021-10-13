a2ae69cf9800828134374a2ddf70eed7a81ae73a
Alexandre Wolff <alexandrewlff@gmail.com>
2021-10-13 18:09:40 +0200

    Corrections

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -3,8 +3,10 @@
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Escrow {
+    using SafeERC20 for IERC20;
     IERC20 private token;
 
     mapping(address => mapping(address => uint256)) lockedIn;
@@ -15,17 +17,13 @@ contract Escrow {
     }
 
     function lock(address recipient, uint256 amount) external {
-        require(token.balanceOf(msg.sender) >= amount, 'Insufficient balance');
-        require(token.allowance(msg.sender, address(this)) >= amount, 'Escrow not authorized');
-
-        require(token.transferFrom(msg.sender, address(this), amount), "Could not tranfer tokens");
         lockedIn[msg.sender][recipient] += amount;
+        token.safeTransferFrom(msg.sender, address(this), amount);
     }
 
     function release(address recipient) external {
-        uint256 amount = lockedIn[msg.sender][recipient];
+        released[recipient] = lockedIn[msg.sender][recipient];
         lockedIn[msg.sender][recipient] = 0;
-        released[recipient] += amount;
     }
 
     function lockedInBalance(address sender, address recipient) external view returns (uint256) {
@@ -38,10 +36,7 @@ contract Escrow {
 
     function claim() external {
         uint256 amount = released[msg.sender];
-        if (amount > 0) {
-            released[msg.sender] = 0;
-            require(token.transfer(msg.sender, amount), "Could not transfer merchant's balance");
-        }
+        released[msg.sender] = 0;
+        token.safeTransfer(msg.sender, amount);
     }
-
 }
